546926d964a17073cae601bb85598a66fdba65a7
unknown <luy89@mcmaster.ca>
2017-10-20 14:53:53 -0400

    database methods

--- src/database.py
+++ src/database.py
@@ -1,13 +1,20 @@
 import peewee
 
 db = peewee.SqliteDatabase('pppDatabase.db')
+db.connect()
 
 class Account(peewee.Model):
-    AccID = peewee.FixedCharField(20, unique=True)
+##    AccID = peewee.FixedCharField(20, null=False, unique=True, )
     UserName = peewee.CharField()
     Password = peewee.CharField()
 
     class Meta:
         database = db
-        
     
+def DeleteTable():
+    db.drop_table(Account)
+    db.create_table(Account)
+
+def Insert(U,P):
+    newAcc = Account(UserName=U,Password=P)
+    newAcc.save()
