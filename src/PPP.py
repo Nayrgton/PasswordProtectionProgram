467a919ca5c5da425ff529fb8a63a83d065de2f8
Suhavi Sandhu <sandhs11@mcmaster.ca>
2017-10-31 16:57:36 -0400

    Made login and register frame

--- src/PPP.py
+++ src/PPP.py
@@ -1,35 +1,135 @@
 from tkinter import *
+from tkinter import ttk
 import re
 
 bgc = "#E6E6FA"
+LARGE = ("Helvetica", 16)
+BUTTONS = ("Sans-Serif", 10, "bold")
 
+
+# Class for the GUI wi
 class PPP(Tk):
 
-    def __init__(self, *args, **kwargs):
-        Tk.__init__(self, *args, **kwargs)
-        container = Frame(self)
+    def __init__(self, *args):
+        Tk.__init__(self, *args)
 
-        container.pack(side="top", fill="both", expand = True)
+        self.state = {
+            "prompt": "Enter your master password", "val": False
+            }
 
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+        self.showLogIn()
+        '''
+        if (password in database):
+            self.showLogIn()
+        else:
+            self.showCreateMP()
+        '''    
+        self.title("Password Protection Program")
+        self.geometry("1080x840")
+        # window.wm_iconbitmap('logo.ico')
 
-        self.frames = {}
+    # Show appropriate fram
+    def showLogIn(self, *kwargs):
+        logIn = Frame(self)
+        logIn.pack()
 
-        for F in (HomeScreen, PWPage):
+        welcome = Label(logIn, text="Welcome to the Password Protection Program", font=LARGE)
+        welcome.grid(row=0, columnspan=3)
 
-            frame = F(container, self)
+        enterLabel = Label(logIn, text=self.state['prompt'])
+        enterLabel.grid(row=1, column=1)
 
-            self.frames[F] = frame
+        ent = ttk.Entry(logIn, show="*")
+        ent.grid(row=2, column=1)
+        ent.bind('<Return>', lambda x: self.matchPassword(logIn, label=criteria, entry=ent, btn=btn))
+        ent.focus_set()
 
-            frame.grid(row=0, column=0, sticky="nsew")
+        btn = Button(logIn, text="Submit", command=lambda: self.matchPassword(logIn, label=criteria, entry=ent, btn=btn))
+        btn.grid(row=3, column=1)
+        
+        criteria = Label(logIn, text="")
+        criteria.grid(row=4, column=1)
 
-        self.show_frame(HomeScreen)
+    # Checks if password is good and then encrypts it to a file, additional parameters enterLabel, entry and button
+    def matchPassword(self, frame, **kwargs):
+        password = kwargs['entry'].get()
 
-    def show_frame(self, cont):
+        # Get master password from DB
+        # Unencrypt it
+        mp = "Assssss1"
 
-        frame = self.frames[cont]
-        frame.tkraise()
+        if len(password) == 0:
+            msg = "Password cannot be empty"
+
+        elif password != mp:
+            msg = "Incorrect password"
+
+        elif password == mp:
+            # Destroy the current frame, createMP
+            frame.destroy()
+            # Show the password mangement page
+            self.showPWPage()
+            return
+
+        kwargs['label'].config(text=msg)
+
+    # Checks if password is good and then encrypts it to a file, additional parameters enterLabel, entry and button
+    def checkPassword(self, frame, **kwargs):
+        password = kwargs['entry'].get()
+
+        if len(password) == 0:
+            msg = "Password cannot be empty"
+
+        elif len(password) < 8:
+            msg = "Password must be at least 8 characters"
+
+        elif not(re.search("[0-9]", password)):
+            msg = "Password must have at least 1 number"
+            
+        elif not(re.search("[a-z]", password) and re.search("[A-Z]", password)):
+            msg = "Password must have uppercase and lowercase"
+            
+        else:
+            # Call encryption methods
+            print("Encrypting")
+            # Insert to database method
+            print("Inserting to database")
+            # Destroy the current frame, createMP
+            frame.destroy()
+            # Show the password mangement page
+            self.showPWPage()
+            return
+
+        kwargs['label'].config(text=msg)
+
+    def showCreateMP(self, *args):
+        createMP = Frame(self)
+        createMP.pack()
+
+        welcome = Label(createMP, text="Welcome to the Password Protection Program", font=LARGE)
+        welcome.grid(row=0, columnspan=3)
+
+        enterLabel = Label(createMP, text="Create a master password to start using the application")
+        enterLabel.grid(row=1, column=1)
+
+        ent = ttk.Entry(createMP, show="*")
+        ent.grid(row=2, column=1)
+        ent.bind('<Return>', lambda x: self.checkPassword(createMP, label=criteria, entry=ent, btn=btn))
+        ent.focus_set() # sets focus to entry box
+
+        btn = Button(createMP, text="Submit", command=lambda: self.checkPassword(createMP, label=criteria, entry=ent, btn=btn))
+        btn.grid(row=3, column=1)
+        
+        criteria = Label(createMP, text="")
+        criteria.grid(row=4, column=1)
+
+    def showPWPage(self, *args):
+        PWPage = Frame(self)
+        PWPage.pack()
+
+        w = Label(PWPage, text="smd")
+        w.grid(row=0, columnspan=3)
+        
 
 class HomeScreen(Frame):
 
@@ -39,32 +139,14 @@ class HomeScreen(Frame):
         welcome = Label(self, text="Welcome to the Password Protection Program", bg=bgc, font=("Helvetica",16))
         welcome.pack(pady=10,padx=10)
 
+        enterLabel = Label(self, text="Enter a master password to keep all your usernames and passwords secure")
+        enterLabel.pack()
+
         # Enter password
         ent = Entry(self, show="*")
         ent.pack()
 
-        # Checks if password is good and then encrypts it to a file
-        def checkPassword():
-            password = ent.get()
-            strength = 0
-
-            if len(password) == 0:
-                msg = "Password cannot be empty"
-
-            elif len(password) < 8:
-                msg = "Password must be at least 8 characters"
-
-            elif not(re.search("[0-9]", password)):
-                msg = "Password must have at least 1 number"
-                
-            elif not(re.search("[a-z]", password) and re.search("[A-Z]", password)):
-                msg = "Password must have uppercase and lowercase"
-                
-            else:
-                controller.show_frame(PWPage)
-                return
-
-            mesg.configure(text=msg)
+       
         
         # Button
         btn = Button(self, text="Enter", font=("Helvetica",16), command=checkPassword)
@@ -74,6 +156,13 @@ class HomeScreen(Frame):
         mesg = Label(self, text="", bg=bgc)
         mesg.pack()
 
+        welcome.place(relx=.5, rely=.3, anchor="c")
+        enterLabel.place(relx=.5, rely=.45, anchor="c")
+        ent.place(relx=.5, rely=.5, anchor="c")
+        btn.place(relx=.5, rely=.55, anchor="c")
+        mesg.place(relx=.5, rely=.6, anchor="c")
+
+
         
 class PWPage(Frame):
 
@@ -82,18 +171,35 @@ class PWPage(Frame):
         welcome = Label(self, text="Add an entry to the PasswordPotatoProgram", bg=bgc, font=("Helvetica",16))
         welcome.pack(pady=10,padx=10)
 
-        plus = Button(self, text="+", command=lambda: showEntry())
-        plus.pack()
 
-        def showEntry():
-            typeLabel = Label(self, text="What type of account is this?")
-            typeLabel.pack()
+        nameLabel = Label(self, text="Name")
+        nameLabel.pack()
+        
+        typeLabel = Label(self, text="Type")
+        typeLabel.pack()
 
-            typeEnt = Entry(self)
-            typeEnt.pack()
+        typeEnt = Entry(self)
+        typeEnt.pack()
 
-            userLabel = Label(self, text="Username")
-            userLabel.pack()
+        userLabel = Label(self, text="Username")
+        userLabel.pack()
+
+        userEnt = Entry(self)
+        userEnt.pack()
+
+        passLabel = Label(self, text="Password")
+        passLabel.pack()
+
+        passEnt = Entry(self, show="*")
+        passEnt.pack()
+
+        addButton = Button(self, text="Add")
+        addButton.pack()
+
+
+                
+
+            
 
 app = PPP()
 
