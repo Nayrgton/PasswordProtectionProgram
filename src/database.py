18abf268a93cb8773608d19283f8bbe2b0143f1e
unknown <luy89@mcmaster.ca>
2017-11-04 16:58:44 -0400

    tested and complete

--- src/database.py
+++ src/database.py
@@ -21,10 +21,10 @@ class BaseModel(peewee.Model):
 #  @param AccType Type of Account used
 #  @param UserName Account Username
 class Account(BaseModel):
-    AccID = peewee.PrimaryKeyField()
+    ID = peewee.PrimaryKeyField()
     AccName = peewee.CharField()
     AccType = peewee.CharField()
-    UserName = peewee.CharField()
+    UserName = peewee.CharField(null=True)
 
 ## @brief SQLite table to store hash keys and hash values
 #  @detail use peewee orm library to create a table class that stores hash values and hash keys in a database. This table is not accessabile via the application. 
@@ -32,16 +32,19 @@ class Account(BaseModel):
 #  @param HashVal Hashed value of Password
 #  @param HashKey Key to Decrypt Password
 class Encrypt(BaseModel):
-    Eid = peewee.ForeignKeyField(Account, on_field='AccID', primary_key=True, on_delete='CASCADE')
-    HashVal = peewee.CharField(True, unique=True)
+    ID = peewee.ForeignKeyField(Account, to_field='ID', primary_key=True, on_delete='CASCADE')
+    HashVal = peewee.CharField(10, unique=True)
     HashKey = peewee.FixedCharField(10)
 
-## @brief Instantiate new empty table
+## @brief Instantiate new empty tables
 #  @detail Encrypt Table should also be reset when Account is reset
-def ResetTable():
-    db.drop_table(Account)
-    db.create_table(Account)
-    db.create_table(Encrypt)
+def CreateTables():
+    db.create_tables([Account, Encrypt], safe=True)
+
+## @brief Delete tables
+#  @detail Encrypt Table should also be reset when Account is reset
+def DropTables():
+    db.drop_tables([Account, Encrypt], safe=True)
 
 ## @brief Insert new Account Instance and Encrypt Instance
 #  @param N Account Name
@@ -49,39 +52,40 @@ def ResetTable():
 #  @param U username
 #  @param Hv Hash Value
 #  @param Hk Hash Key
-def Insert(N, T, U, Hv, Hk):
-    Account(AccName=N, AccType=T, UserName=U).save()
-    Encrypt(Accname=Hv, HashKey=Hk).save()
+def Insert(Id, N, T, U, Hv, Hk):
+    Account.create(ID = Id, AccName=N, AccType=T, UserName=U)
+    Encrypt.create(ID=Id, HashVal=Hv, HashKey=Hk)
 
 ## @brief search tables with AccId
 #  @param Id Account Id
 def GetId(Id):
-    return Account.get(AccID.id == Id), Encrypt.get(Eid == Id)
+    return Account.select().join(Encrypt).where(Account.ID == Id)[0]
 
 ## @brief Get Table Rows with Account Type
 #  @param Atype Account type
 def GetT(Atype):
-    return Account.select(Account.AccType == Atype)
+    return Account.select().join(Encrypt).where(Account.AccType == Atype)
 
 ## @brief Get Table Row with Account name
 #  @param Aname Account Name
-def GetU(Aname):
-    return Account.select(Account.AccName == name)
+def GetN(Aname):
+    return Account.select().join(Encrypt).where(Account.AccName == Aname)
 
 ## @brief Delete Table Row with ID
 #  @param Aid Account ID
 def Delete(Aid):
-    Account.delete().where(Account.AccID == Aid).execute()
+    Account.delete().where(Account.ID == Aid).execute()
+    Encrypt.delete().where(Encrypt.ID == Aid).execute()
 
 ## @brief Update Table Row with ID
 #  @param Aid Account Id
 #  @param U new Username
 #  @param Hv new Hash Value
 def UpdateU(Aid, U):
-    Account.update(UserName=U).where(Account.Aid == Aid).execute()
+    Account.update(UserName=U).where(Account.ID == Aid).execute()
 
 ## @brief Update Password with ID
 #  @param Aid Account Id (Encrypted ID)
 #  @param Hv new Hash Value
 def UpdateP(Aid, Hv):
-    Encrypt.update(HashVal=Hv).where(Encrypt.Eid == Aid).execute()
+    Encrypt.update(HashVal=Hv).where(Encrypt.ID == Aid).execute()
