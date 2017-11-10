6aaa95df48fd1facffeb151adba4baf65e41709a
Shabana Dhayananth <dhayanas@mcmaster.ca>
2017-11-10 18:46:11 -0500

    commented encrypt for understanding

--- src/Encrypt.py
+++ src/Encrypt.py
@@ -11,11 +11,12 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 ## @brief This function generates a unique) key (for encoding user passwords) using Python's Fernet library
-#  @details key is a  
-#  @return 
+#  @details key derived from a string that is run through the kdf (key derivation function)
+#  @return key that will be used to encode the user passwords (32 bytes)
 def generKey():
     password = b"passprotprog" #what you want to encode with
     salt = os.urandom(16)
+    #PBKDF2HMAC derives cryptographic key from password by key stretching
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=32,
@@ -24,20 +25,22 @@ def generKey():
         backend=default_backend()
      )
     key = base64.urlsafe_b64encode(kdf.derive(password)) #need to store this somewhere (in database) to be able to decrypt  
-    print (key)
-    #f = Fernet(key)
     return key
 
 ## @brief This function uses a key to encrypt an input string
-#  @details 
+#  @details Fernet uses symmetric encryption on the input key
 #  @param key, passw refer to the key to be used to encypt and the password to be encrypted
-#  @return encrypted password (string of Unicode characters)
+#  @return encrypted password 
 def cryptEncode(key, passw):
     f = Fernet(key)
     inp = passw.encode(encoding= 'utf-8')
     encrypted = f.encrypt(inp)
     return encrypted
 
+
+## @brief This function uses the saved key to decrypt the encrypted user password stored in the database
+#  @param key, encrypted refer to the key that was used to encrpt the password and the encrypted password
+#  @return decrypted password
 def cryptDecode(key, encrypted):
     f = Fernet(key)
     decrypted = f.decrypt(encrypted)
@@ -49,9 +52,5 @@ key = generKey()
 x = cryptEncode(key, "hellooo")
 a = cryptDecode(key, x)
 '''
-
-#infile = open("Passwords.txt","r").read().strip().split("\n")
-#for i in infile:
-#    print(token(i))
 
 
