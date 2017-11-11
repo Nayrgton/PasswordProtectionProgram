83775171b3319914c6381c843f581cf81e43b9ad
Shabana Dhayananth <dhayanas@mcmaster.ca>
2017-11-10 19:01:50 -0500

    added comments to passGen

--- src/GenPassword.py
+++ src/GenPassword.py
@@ -1,8 +1,20 @@
+## @file GenPassword.py
+#  @title Generate Random Password
+#  @author Shabana Dhayananth
+#  @date October 27, 2017
+
 import string
+import sys
 import random
 
+## @brief This function generates a random password consisting of upper case, lower case alphanumeric characters
+#  @details default random number generator's sequences can be reproduced, in case SystemRandom() is not available on user system
+#  @return random password consisting of 8 characters
 def genPass():
     return ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(8))
 
+## @brief This function generates a random password consisting of upper case, lower case alphanumeric characters
+#  @details Same as genPass() but uses SystemRandom() to generate random numbers so sequences are not reproducible
+#  @return random password consisting of 8 characters
 def genPassCrypt():
    return ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(8)) 
