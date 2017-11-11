52efe2a50c3c12a06f8a1d7c8bc612485bc80650
Suhavi Sandhu <sandhs11@mcmaster.ca>
2017-11-10 22:21:24 -0500

    Integrated database with GUI

--- src/PPP.py
+++ src/PPP.py
@@ -26,43 +26,32 @@ class PPP(Tk):
     def __init__(self, *args):
 
         # Initialize GUI
-        Tk.__init__(self, *args)
-        
-
-        # States of the GUI that are used in multiple methods
-        self.state = {
-            "prompt": "Enter your master password", "val": False
-            }
-        # List of accounts
-        self.accounts = []
-
-        # Checks if master password has been initialized or not
-        db = database.Account.select()
-        if (len(db)):
-            self.showLogIn()
-        else:
-            self.showCreateMP()
-        
-
+        Tk.__init__(self, *args)        
         # Customizing window title, size and icon
         self.title("Password Protection Program")
         self.geometry("1080x840")
         self.configure(background=BGC)
         # self.wm_iconbitmap('logo.ico')
+        # List of accounts
+        self.accounts = []
+
+        # Checks if master password has been initialized or not
+        db = database.Account.select()
+        if (len(db) == 0):
+            self.showCreateMP()
+        else:
+            self.showLogIn()
 
     ## @brief Log In Screen
     #  @details Displays the log in frame
     #  @param *kwargs A variable argument list to provide extra functionality to the frame
     def showLogIn(self, *kwargs):
-
-        # Creatiing and adding the frame
-        logIn = Frame(self, bg=BGC)
-        #logIn.pack()
-        
+        # Creating and adding the frame
+        logIn = Frame(self, bg=BGC)        
         welcome = Label(logIn, text="Welcome to the Password Protection Program", fg=BG, bg=BGC, font=LARGE)
         welcome.grid(row=0, columnspan=3, padx=10, pady=10) # Divides space into 3 columns and places label in the middle
 
-        enterLabel = Label(logIn, fg=BG, bg=BGC, text=self.state['prompt'])
+        enterLabel = Label(logIn, fg=BG, bg=BGC, text="Enter your master password")
         enterLabel.grid(row=1, column=1, padx=10, pady=2)
 
         ent = ttk.Entry(logIn, width=10, font=LARGE, show="*")
@@ -88,10 +77,7 @@ class PPP(Tk):
     def matchPassword(self, frame, **kwargs):
         password = kwargs['entry'].get()
 
-        # Get master password from DB
-        # Unencrypt it
-        # Master password from DB
-        ## JOSEPH ##
+        # Get master password from DB and decrypt it
         mp = database.GetId(1).HashVal # remember to change this when encryption works
         
         # Makes sure the user entered the correct password
@@ -120,20 +106,17 @@ class PPP(Tk):
         # Similarly to showLogIn method, creates and adds widgets to frame
         createMP = Frame(self)
         createMP.pack()
-
         welcome = Label(createMP, text="Welcome to the Password Protection Program", font=LARGE)
         welcome.grid(row=0, columnspan=3)
-
         enterLabel = Label(createMP, text="Create a master password to start using the application")
         enterLabel.grid(row=1, column=1)
-
         ent = ttk.Entry(createMP, show="*")
         ent.grid(row=2, column=1)
         ent.bind('<Return>', lambda x: self.checkPassword(createMP, label=criteria, entry=ent))
         ent.focus_set() # sets focus to entry box
 
         # When clicked, matchPassword method gets called with the frame name, label and the password entered by user
-        btn = Button(createMP, text="Submit", command=lambda: self.checkPassword(createMP, label=criteria, entry=ent))
+        btn = Button(createMP, text="Submit", command=lambda: self.checkMP(createMP, label=criteria, entry=ent))
         btn.grid(row=3, column=1)
 
         # Empty label that gets updated to show whether the password is strong enough
@@ -141,44 +124,31 @@ class PPP(Tk):
         criteria.grid(row=4, column=1)
 
         
-    ## @brief Check Password
+    ## @brief Check Master Password
     #  @details Checks if password meets criteria for master password creation
     #  @param frame The frame which called the method, so it can be updated to show something else upon entering a satisfatory password
     #  @param **kwargs A variable argument list, in this case takes the label that tells you if incorrect password and entry from user
-    def checkPassword(self, frame, **kwargs):
+    def checkMP(self, frame, **kwargs):
         # Get password
         password = kwargs['entry'].get()
-    
-        if len(password) == 0:
-            msg = "Password cannot be empty"
+        response = PWChecking.checkMP(password)
 
-        elif len(password) < 8:
-            msg = "Password must be at least 8 characters"
-
-        elif not(re.search("[0-9]", password)):
-            msg = "Password must have at least 1 number"
-            
-        elif not(re.search("[a-z]", password) and re.search("[A-Z]", password)):
-            msg = "Password must have uppercase and lowercase"
-
+        # If response is string, that means something is wrong with the input (not long enough, etc)
+        if type(response) == str:
+            # Updates label with correct response
+            kwargs['label'].config(text=msg)
         # Password satisfies constraints    
         else:
             # Call encryption methods
             enV = password
-            enK = "Encrypting"
-            
+            enK = "Encrypting"            
             # Insert to database method
-            ## JOSEPH ##
             database.Insert(1, "Master", "Master", "", enV, enK)
-
             # Destroy the current frame, createMP
             frame.destroy()
             # Show the password mangement page
             self.showPWPage()
             return
-
-        # Updates label with correct response
-        kwargs['label'].config(text=msg)
         
     def showEntry(self, frame, detailFrame):
         entries = []
---
+++ src/PWChecking.py
@@ -0,0 +1,19 @@
+def checkMP(password):
+    if len(password) == 0:
+        return "Password cannot be empty"
+
+    elif len(password) < 8:
+        return "Password must be at least 8 characters"
+
+    elif not(re.search("[0-9]", password)):
+        return "Password must have at least 1 number"
+            
+    elif not(re.search("[a-z]", password) and re.search("[A-Z]", password)):
+        return "Password must have uppercase and lowercase"
+
+    else:
+        return True
+
+
+def checkLogIn(password):
+    
