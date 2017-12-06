d035ced75abf628c581894f44e990312d07168cc
unknown <luy89@mcmaster.ca>
2017-12-06 18:10:57 -0500

    added doxygen comments

--- src/testDatabase.py
+++ src/testDatabase.py
@@ -7,16 +7,19 @@ import unittest
 import Encrypt
 import database
 
-## @brief
+## @brief This class is used to test the functions in database.py
 class testDatabase(unittest.TestCase):
 
+    ## @brief This method creates a list of keys using the Encrypt module as well as create empty tables
     def setUp(self):
         self.keys = [Encrypt.generKey() for i in range(3)]
         database.CreateTables()
 
+    ## @brief This method deletes the tables in the database
     def tearDown(self):
         database.DropTables()
 
+    ## @brief This method tests the Get functions by checking that all Get functions return equal outputs "Test" 
     def test_FRDB4(self):
         database.Insert("First","Test","", Encrypt.cryptEncode(self.keys[0],"First123"), self.keys[0])
         database.Insert("Second","Test","First", Encrypt.cryptEncode(self.keys[1], "Second1"), self.keys[1])
@@ -25,16 +28,17 @@ class testDatabase(unittest.TestCase):
         All = database.GetAll()
         Types = database.GetT("Test")
         First_id = database.GetId(1)
-	First_Name = database.GetN("First")
+        First_Name = database.GetN("First")
 
-	self.assertEqual(len(All), 3)
-	self.assertEqual(len(Types), 3)
-	self.assertEqual(All[0], First_id)
-	self.assertEqual(All[0], First_Name)
-	self.assertEqual(Types[0], First_id)
-	self.assertEqual(Types[0], First_Name)
-	[self.assertEqual(x.AccType, "Test") for x in Types]
+        self.assertEqual(len(All), 3)
+        self.assertEqual(len(Types), 3)
+        self.assertEqual(All[0], First_id)
+        self.assertEqual(All[0], First_Name)
+        self.assertEqual(Types[0], First_id)
+        self.assertEqual(Types[0], First_Name)
+        [self.assertEqual(x.AccType, "Test") for x in Types]
 
+    ## @brief THis method tests the insertion into an empty database, as this is the master password
     def test_FRDB1(self):
         database.Insert("First","","", Encrypt.cryptEncode(self.keys[0],"First123"), self.keys[0])
 
@@ -44,6 +48,7 @@ class testDatabase(unittest.TestCase):
         self.assertNotEqual(First.HashVal, "First123") # if master password is directly in database
         self.assertEqual(First.ID, 1)             # if master password has 1 as ID.
 
+    ## @brief This method tests the Update password Function
     def test_FRDB2_1(self):
         database.Insert("First","","", Encrypt.cryptEncode(self.keys[0],"First123"), self.keys[0])
         database.Insert("Second","","First", Encrypt.cryptEncode(self.keys[1], "Second1"), self.keys[1])
@@ -57,6 +62,7 @@ class testDatabase(unittest.TestCase):
         self.assertEqual(oldQuery.ID, newQuery.ID)
         self.assertNotEqual(Encrypt.cryptDecode(self.keys[0],oldQuery.HashVal.encode('utf-8')), Encrypt.cryptDecode(self.keys[0],newQuery.HashVal.encode('utf-8')))
 
+    ## @brief This function tests the Update username Function
     def test_FRDB2_2(self):
         database.Insert("First","","", Encrypt.cryptEncode(self.keys[0],"First123"), self.keys[0])
         database.Insert("Second","","First", Encrypt.cryptEncode(self.keys[1], "Second1"), self.keys[1])
@@ -70,6 +76,7 @@ class testDatabase(unittest.TestCase):
         self.assertEqual(oldQuery.ID, newQuery.ID)
         self.assertNotEqual(oldQuery.UserName, newQuery.UserName)
     
+    ## @brief This function test the Delete row Function.
     def test_FRDB3(self):
         database.Insert("First","","", Encrypt.cryptEncode(self.keys[0],"First123"), self.keys[0])
         database.Insert("Second","","First", Encrypt.cryptEncode(self.keys[1], "Second1"), self.keys[1])
